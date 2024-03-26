edb7850dccfa26909a1eb048aa59bc2b6f2fc838
brycoti <bryancozartitos@gmail.com>
2024-03-26 16:31:04 +0100

    netlify added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
 import node from '@astrojs/node';
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'middleware',
-  }),
+  adapter: netlify(),
   integrations: [tailwind()]
 });
