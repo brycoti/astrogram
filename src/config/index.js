3783af8bd73639f40e33ae97d7002ed5c34b8195
brycoti <bryancozartitos@gmail.com>
2024-03-26 16:10:54 +0100

    first commit

---
+++ src/config/index.js
@@ -0,0 +1,9 @@
+const IP = "159.223.31.62";
+
+export default {
+  //  API_URL: `https://ricardhernandez//${IP}:3000/api`,
+   // IMG_URL: `https://ricardhernandez//${IP}:3000/img`,
+
+   API_URL: `https://ricardhernandez.com/api`,
+   IMG_URL: `https://ricardhernandez.com/img`,
+}
